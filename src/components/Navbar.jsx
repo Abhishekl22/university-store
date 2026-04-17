156c6df5451e6ef000eabc82d6755d09a08fc3eb
Abhishekl22 <abhishekrana22da@gmail.com>
2026-04-17 18:30:00 +0530

    Fix build configuration and add vercel.json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 // Replace the brands import with this
+// Change this line:
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
